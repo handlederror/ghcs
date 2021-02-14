ca989e91981ab38c07b53491f23c268b962579f5
handlederror <madev.na.ft@gmail.com>
2021-02-14 17:23:53 +0300

    Added server initialization and server loading

--- src/com/github/handlederror/ghcs/client/Main.java
+++ src/com/github/handlederror/ghcs/client/Main.java
@@ -3,10 +3,12 @@ package com.github.handlederror.ghcs.client;
 import java.io.File;
 import java.util.Scanner;
 import java.io.IOException;
+import com.github.handlederror.ghcs.server.Error;
 import com.github.handlederror.ghcs.server.Configuration;
 import static com.github.handlederror.JColored.Colored.bg;
 import static com.github.handlederror.JColored.Colored.fg;
 import static com.github.handlederror.JColored.Colored.stylize;
+import com.github.handlederror.CommandExecutor.CommandExecutor;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -25,13 +27,38 @@ public class Main {
             if (input.equals("exit")) {
                 getInput = false;
             } else {
-                if (input.equals("new")) {
+                if (input.equals("new file")) {
                     System.out.print(stylize("File ", fg("yellow")) + stylize("? ", fg("green")));
                     System.out.print(stylize("", fg("reset"), false));
                     input = scanner.nextLine();
                     
                     if (configuration.serverLocation != null) {
                         File file = new File(configuration.serverLocation + "/" + input);
+
+                        Boolean fileCreate = file.createNewFile();
+                        fileCreate = null;
+
+                        System.gc();
+                    } else {
+                        Error.raise("ServerNotFound", "Initialize server with command `new server' or can't run command", line, 1);
+                    }
+                } else {
+                    if (input.equals("new server")) {
+                        System.out.print(stylize("Directory ", fg("yellow")) + stylize("? ", fg("green")));
+                        System.out.print(stylize("", fg("reset"), false));
+                        input = scanner.nextLine();
+
+                        CommandExecutor.executeCommand(new String[]{"git", "init", input});
+
+                        configuration.serverLocation = input;
+                    } else {
+                        if (input.equals("load server")) {
+                            System.out.print(stylize("Directory ", fg("yellow")) + stylize("? ", fg("green")));
+                            System.out.print(stylize("", fg("reset"), false));
+                            input = scanner.nextLine();
+
+                            configuration.serverLocation = input;
+                        }
                     }
                 }
             }
--- src/com/github/handlederror/ghcs/server/Error.java
+++ src/com/github/handlederror/ghcs/server/Error.java
@@ -4,7 +4,9 @@ import static com.github.handlederror.JColored.Colored.fg;
 import static com.github.handlederror.JColored.Colored.stylize;
 
 public class Error {
-    public static void raise(String errorName, String errorDescription, Integer errorLine) {
+    public static void raise(String errorName, String errorDescription, Integer errorLine, Integer exitCode) {
+        System.out.println(stylize(errorName, fg("red")) + ": " + stylize(errorDescription, fg("white")) + " at " + stylize(errorLine.toString(), fg("yellow")) + ".");
 
+        System.exit(exitCode);
     }
 }
